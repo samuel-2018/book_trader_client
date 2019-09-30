83c7727a32e6ee8636eee0ffe9fd25990dc359c3
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-09-30 08:47:17 -0400

    changed JSX box structure and class names, changed click handling, added api call for getting owner data when no books returned

--- src/components/UserBooks.js
+++ src/components/UserBooks.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 import { Context } from "../contexts/globalContext";
 import { Book } from "./Book";
 
+// Shows all books from one user.
+
 class UserBooks extends React.Component {
   static contextType = Context;
   constructor() {
     super();
-    this.state = { books: [] };
+    this.state = { books: [], username: "" };
   }
 
   componentDidMount() {
-    // Calls API, updates state with course data.
+    // Calls API, updates state with book data.
     // (Updating state must be done outside of render.)
     (() => {
       this.context
@@ -22,51 +24,71 @@ class UserBooks extends React.Component {
         .then(result => {
           // Stores data in state.
           this.setState({
-            books: result.data
+            books: result.data,
+            username: result.data[0].owner.username
           });
         })
         .catch(error => {
-          this.context.handleError.call(this, { error });
+          // If no books, get owner data.
+          // (Needed to get username.)
+          if (error.response.status === 404) {
+            this.context
+              .sendRequest({
+                url: `/users/${this.props.match.params.id}`,
+                method: "GET"
+              })
+              .then(result => {
+                // Stores data in state.
+                this.setState({
+                  username: result.data.username
+                });
+              })
+              .catch(error => {
+                this.context.handleError.call(this, { error });
+              });
+          } else {
+            this.context.handleError.call(this, { error });
+          }
         });
     })();
   }
 
+  onClickNew = () => {
+    this.props.history.push("/newbook");
+  };
+
   render() {
     return (
-      <>
-        {this.state.books.length ? (
-          <>
+      <div className="bounds-content">
+        {this.state.username !== "" ? (
+          <div className="wrapper__100">
             {/* Page Title */}
             <div className="page-title">
-              <h1>{this.state.books[0].owner.username}'s Books</h1>
+              <h1>{this.state.username}'s Books</h1>
             </div>
 
             {/* Books List */}
-            <div>
-              {this.state.books.map(book => {
-                return <Book key={`book-${book.bookId}`} bookData={book} />;
-              })}
-            </div>
+            {this.state.books.length ? (
+              <div className="page-body">
+                {this.state.books.map(book => {
+                  return <Book key={`book-${book.bookId}`} bookData={book} />;
+                })}
+              </div>
+            ) : (
+              <div className="box-title request__body request message-box__full">
+                Nothing Found
+              </div>
+            )}
 
             {/* Create New Book Button */}
-            <div className="button">
-              <Link
-                to={{
-                  pathname: `/newbook`,
-                  state: { from: this.props.location }
-                }}
-              >
-                <h3>
-                  <i className="fas fa-plus"></i>
-                  Create New Book
-                </h3>
-              </Link>
-            </div>
-          </>
+            <button className="button" onClick={this.onClickNew}>
+              <i className="fas fa-plus"></i> Create New Book
+            </button>
+          </div>
         ) : (
-          ""
+          <div className="box-title request__body request">Loading...</div>
         )}
-      </>
+      </div>
     );
   }
 }

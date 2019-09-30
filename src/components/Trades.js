d13961dcb738cee6712df370f2f4cc5ffb658119
samuel-2018 <45561451+samuel-2018@users.noreply.github.com>
2019-09-30 08:45:45 -0400

    added core component content

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,16 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { Trade } from "./Trade";
 import { Context } from "../contexts/globalContext";
 
 class Trades extends React.Component {
   static contextType = Context;
   constructor() {
     super();
-    this.state = {};
+    this.state = { trades: [] };
+  }
+  componentDidMount() {
+    // Calls API, updates state with request data.
+    // (Updating state must be done outside of render.)
+    this.loadTrades();
+  }
+
+  loadTrades = () => {
+    this.context
+      .sendRequest({ url: "/trades", method: "GET" })
+      .then(result => {
+        // Stores data in state.
+        this.setState({
+          trades: result.data
+        });
+      })
+      .catch(error => {
+        this.context.handleError.call(this, { error });
+      });
+  };
+
+  getTrades(tradeList) {
+    return tradeList.length ? (
+      tradeList.map(trade => {
+        return <Trade key={`trade-${trade.tradeId}`} tradeData={trade} />;
+      })
+    ) : (
+      <div className="box-title request__body request message-box__partual">
+        Nothing Found
+      </div>
+    );
   }
 
   render() {
-    return <div>Trades</div>;
+    return (
+      <div className="bounds-content">
+        {/* Section Title */}
+        <div className="page-title">
+          <h1>Trades</h1>
+        </div>
+
+        <div className="page-body">
+          {/* Trade List */}
+          <div className="page-body">{this.getTrades(this.state.trades)}</div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,16 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { Context } from "../contexts/globalContext";
+import { User } from "./User";
 
 class Users extends React.Component {
   static contextType = Context;
   constructor() {
     super();
-    this.state = {};
+    this.state = { users: [] };
+  }
+
+  componentDidMount() {
+    // Calls API, updates state with user data.
+    // (Updating state must be done outside of render.)
+    (() => {
+      this.context
+        .sendRequest({ url: "/users/all", method: "GET" })
+        .then(result => {
+          // Stores data in state.
+          this.setState({
+            users: result.data
+          });
+        })
+        .catch(error => {
+          this.context.handleError.call(this, { error });
+        });
+    })();
   }
 
   render() {
-    return <div>Users</div>;
+    return (
+      <div className="bounds-content">
+        {/* Page Title */}
+        <div className="page-title">
+          <h1>Users</h1>
+        </div>
+
+        {/* User List */}
+        <div className="page-body two-column-wrapper">
+          {this.state.users.map(user => {
+            return <User key={`user-${user.userId}`} userData={user} />;
+          })}
+        </div>
+      </div>
+    );
   }
 }
 
